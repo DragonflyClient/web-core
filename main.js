05aa07318653e186e8cd64c5e0d3a6e0df47b623
flaaaps <flvvpss@gmail.com>
2020-04-14 20:56:16 +0200

    Improved Responsiveness

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const nav = document.querySelector('.nav');
 const ham = document.querySelector('.ham-wrapper');
 const socials = document.querySelector('.socials');
+const landing = document.querySelector('.landing');
+const vid = document.querySelector('.landing-vid');
+
+const video = document.querySelector('video');
+
+vid.addEventListener(
+  'loadedmetadata',
+  (event) => {
+    vid.currentTime = 8;
+  },
+  false
+);
+
 nav.classList.remove('nav-active');
 ham.classList.remove('ham-active');
 
@@ -10,7 +23,16 @@ ham.addEventListener('click', function (event) {
   socials.classList.toggle('socials-active');
   event.preventDefault();
 });
-
+const i = 0;
+landing.addEventListener('click', function () {
+  if (i == 0) {
+    vid.pause();
+    i = 1;
+  } else {
+    vid.play();
+    i = 0;
+  }
+});
 function closeMenu(e) {
   nav.classList.toggle('nav-active');
   ham.classList.toggle('ham-active');
