ab4b07f30b42dc70fe4fa3a3469697c7e8f6da1b
flaaaps <58341821+flaaaps@users.noreply.github.com>
2020-04-25 00:22:07 +0200

    Included changing navbar on scroll

--- main.js
+++ main.js
@@ -61,3 +61,17 @@ accordionHeader.forEach((accordionHeader) => {
     }
   });
 });
+
+// Change Navbar on scroll
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.height = "75px";
+    document.getElementsByClassName("nav").style.fontSize = "55px";
+  } else {
+    document.getElementById("navbar").style.height = "100px";
+    document.getElementsByClassName("nav").style.fontSize = "55px";
+  }
+}
