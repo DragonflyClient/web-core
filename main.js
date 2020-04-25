e30b3d1aa416d1565c2777aeb1ab0acc0f2550f6
flaaaps <58341821+flaaaps@users.noreply.github.com>
2020-04-26 01:02:57 +0200

    Pause/Play Background video

--- main.js
+++ main.js
@@ -18,46 +18,56 @@ ham.addEventListener('click', toggleNav);
 
 /* Functions */
 function toggleNav() {
-  nav.classList.toggle('nav-active');
-  ham.classList.toggle('ham-active');
-  socials.classList.toggle('socials-active');
-}
-function closeMenu(e) {
-  nav.classList.toggle('nav-active');
-  ham.classList.toggle('ham-active');
+    nav.classList.toggle('nav-active');
+    ham.classList.toggle('ham-active');
+    socials.classList.toggle('socials-active');
 }
 
-window.addEventListener('load', function () {
-  const loader = document.querySelector('.pre-loader');
-  loader.classList.add('pl-hide');
+function closeMenu(e) {
+    nav.classList.toggle('nav-active');
+    ham.classList.toggle('ham-active');
+}
+
+window.addEventListener('load', function() {
+    const loader = document.querySelector('.pre-loader');
+    loader.classList.add('pl-hide');
 });
+// Toggle Video Status
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause()
+    }
+}
 // Twenty Twenty only Laptop +
 var width = window.innerWidth;
 if (width > 1000) {
-  injectScript('twentytwenty-master/js/jquery.event.move.js');
-  injectScript('./twentytwenty-master/js/jquery.twentytwenty.js');
+    injectScript('twentytwenty-master/js/jquery.event.move.js');
+    injectScript('./twentytwenty-master/js/jquery.twentytwenty.js');
 }
+
 function injectScript(source) {
-  script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.onload = function () {
-    console.log('Script with source' + source + ' was successfully injected!');
-  };
-  script.src = source;
-  document.getElementsByTagName('head')[0].appendChild(script);
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.onload = function() {
+        console.log('Script with source' + source + ' was successfully injected!');
+    };
+    script.src = source;
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
 // FAQ Accordion
 
 const accordionHeader = document.querySelectorAll('.accordion-item-header');
 
 accordionHeader.forEach((accordionHeader) => {
-  accordionHeader.addEventListener('click', (event) => {
-    accordionHeader.classList.toggle('accordion-active');
-    const accordionItemBody = accordionHeader.nextElementSibling;
-    if (accordionHeader.classList.contains('accordion-active')) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
-    } else {
-      accordionItemBody.style.maxHeight = 0;
-    }
-  });
+    accordionHeader.addEventListener('click', (event) => {
+        accordionHeader.classList.toggle('accordion-active');
+        const accordionItemBody = accordionHeader.nextElementSibling;
+        if (accordionHeader.classList.contains('accordion-active')) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+    });
 });
