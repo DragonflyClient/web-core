9b06ea0033758eeca93dad1fa0e300a7163d162d
flaaaps <flvvpss@gmail.com>
2020-05-02 18:38:43 +0200

    Design Improvements

--- main.js
+++ main.js
@@ -1,65 +1,90 @@
-/* Selectors */
 
+
+/*---------------*/
+/*== SELECTORS ==*/
+/*---------------*/
+
+/* #region selectors */
 const nav = document.querySelector('.nav');
 const ham = document.querySelector('.ham-wrapper');
 const socials = document.querySelector('.socials');
 const landing = document.querySelector('.landing');
 const vid = document.querySelector('.landing-vid');
-
 const video = document.querySelector('video');
+const accordionHeader = document.querySelectorAll('.accordion-item-header');
+const width = window.innerWidth;
+/* #endregion */
 
+
+/*------------*/
+/*== FIELDS ==*/
+/*------------*/
+
+/* #region fields */
+let withTwentyTwenty = false
+/* #endregion */
+
+
+/*---------------------*/
+/*== MAIN STATEMENTS ==*/
+/*---------------------*/
+
+/* #region main-statements */
 vid.currentTime = 8;
 
 nav.classList.remove('nav-active');
 ham.classList.remove('ham-active');
 
-/* Eventlisteners */
-ham.addEventListener('click', toggleNav);
-
-/* Functions */
-function toggleNav() {
-    nav.classList.toggle('nav-active');
-    ham.classList.toggle('ham-active');
-    socials.classList.toggle('socials-active');
-}
-
-function closeMenu(e) {
-    nav.classList.toggle('nav-active');
-    ham.classList.toggle('ham-active');
-}
-
-window.addEventListener('load', function() {
-    const loader = document.querySelector('.pre-loader');
-    loader.classList.add('pl-hide');
-});
-// Toggle Video Status
-function toggleVideoStatus() {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause()
-    }
-}
-// Twenty Twenty only Laptop +
-var width = window.innerWidth;
+// Twenty Twenty only Laptop+
 if (width > 1000) {
     injectScript('twentytwenty-master/js/jquery.event.move.js');
     injectScript('./twentytwenty-master/js/jquery.twentytwenty.js');
+    withTwentyTwenty = true
 }
+/* #endregion */
 
-function injectScript(source) {
-    script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.onload = function() {
-        console.log('Script with source' + source + ' was successfully injected!');
-    };
-    script.src = source;
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
+
+/*-------------------*/
+/*== WINDOW EVENTS ==*/
+/*-------------------*/
+
+/* #region window-events */
+// Autostart Download
+window.addEventListener("load", () => {
+    const downloadLink = $("a[data-download]")[0];
+    setTimeout(() => {
+        window.location = downloadLink.getAttribute("href");
+        console.log("Hello");
+    }, 2000);
+});
+
+// Resize
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 1000 && !withTwentyTwenty) {
+        console.log("Injecting script to use TwentyTwenty since the width was increased.");
+        refresh();
+        withTwentyTwenty = true;
+    } else if (window.innerWidth <= 1000 && withTwentyTwenty) {
+        console.log("Reloading document to remove TwentyTwenty since the width was decreased.");
+        refresh();
+        withTwentyTwenty = false;
+    }
+});
+
+// Pre-Loader
+window.addEventListener('load', function () {
+    const loader = document.querySelector('.pre-loader');
+    loader.classList.add('pl-hide');
+});
+/* #endregion */
+
+
+/*------------------*/
+/*== OTHER EVENTS ==*/
+/*------------------*/
+
+/* #region other-events */
 // FAQ Accordion
-
-const accordionHeader = document.querySelectorAll('.accordion-item-header');
-
 accordionHeader.forEach((accordionHeader) => {
     accordionHeader.addEventListener('click', (event) => {
         accordionHeader.classList.toggle('accordion-active');
@@ -71,3 +96,53 @@ accordionHeader.forEach((accordionHeader) => {
         }
     });
 });
+
+// Hamburger Menu
+ham.addEventListener('click', toggleNav);
+/* #endregion */
+
+
+/*---------------*/
+/*== FUNCTIONS ==*/
+/*---------------*/
+
+/* #region functions */
+// Open / Close the nav menu
+function toggleNav() {
+    nav.classList.toggle('nav-active');
+    ham.classList.toggle('ham-active');
+    socials.classList.toggle('socials-active');
+}
+
+// Close the nav menu
+function closeMenu(e) {
+    nav.classList.toggle('nav-active');
+    ham.classList.toggle('ham-active');
+}
+
+// Refresh the page
+function refresh() {
+    window.location.href = window.location.href;
+}
+
+// Add a script to the document
+function injectScript(source) {
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.onload = function () {
+        console.log('Script with source' + source + ' was successfully injected!');
+    };
+    script.src = source;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+// Pause / Play the background video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause()
+    }
+}
+/* #endregion */
+
