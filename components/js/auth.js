77c85a629ddfd841d1a28b5188ce2df001ac91be
flaps <julianwening4@gmail.com>
2020-08-07 19:14:42 +0200

    Create login page + update auth.js

--- components/js/auth.js
+++ components/js/auth.js
@@ -11,94 +11,97 @@ const Toast = Swal.mixin({
         toast.addEventListener('mouseleave', Swal.resumeTimer)
     }
 })
-formLogin.addEventListener('submit', event => {
-    event.preventDefault()
-    const formDataLogin = new FormData(formLogin)
-    const name = formDataLogin.get("name")
-    const password = formDataLogin.get("password")
-    const body = {
-        "name": name,
-        "password": password
-    }
+if (location.href.indexOf('login') > -1 || location.href.indexOf('ideas') > -1) {
+    formLogin.addEventListener('submit', event => {
+        event.preventDefault()
+        const formDataLogin = new FormData(formLogin)
+        const name = formDataLogin.get("name")
+        const password = formDataLogin.get("password")
+        const body = {
+            "name": name,
+            "password": password
+        }
 
-    fetch("https://api.playdragonfly.net/cookie/login", {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
-    }).then(result => result.json())
-        .then(result => {
-            if (result.success) {
-                console.log(result)
-                const username = result.username
+        fetch("https://api.playdragonfly.net/cookie/login", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: 'include'
+        }).then(result => result.json())
+            .then(result => {
+                if (result.success) {
+                    console.log(result)
+                    const username = result.username
 
-                Toast.fire({
-                    icon: 'success',
-                    title: 'Signed in successfully'
-                })
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Signed in successfully'
+                    })
 
-                document.getElementById('id01').style.display = 'none'
-            } else {
-                const error = result.error
-                console.log(error)
-                Toast.fire({
-                    icon: 'error',
-                    title: result.error
-                })
-            }
-            afterLogin(result.success, result.username)
-        })
-});
+                } else {
+                    const error = result.error
+                    console.log(error)
+                    Toast.fire({
+                        icon: 'error',
+                        title: result.error
+                    })
+                }
+                afterLogin(result.success, result.username)
+            })
+    });
+}
 
-/* Handle register */
-const formRegister = document.getElementById("form-register")
-formRegister.addEventListener('submit', event => {
-    event.preventDefault()
-    const formDataRegister = new FormData(formRegister)
-    console.log(formDataRegister)
-    const name = formDataRegister.get("name")
-    const password = formDataRegister.get("password")
-    const body = {
-        "name": name,
-        "password": password
-    }
+if (location.href.indexOf('register') > -1 || location.href.indexOf('ideas') > -1) {
+    /* Handle register */
+    const formRegister = document.getElementById("form-register")
+    console.log('now')
+    formRegister.addEventListener('submit', event => {
+        event.preventDefault()
+        const formDataRegister = new FormData(formRegister)
+        console.log(formDataRegister)
+        const name = formDataRegister.get("name")
+        const password = formDataRegister.get("password")
+        const body = {
+            "name": name,
+            "password": password
+        }
 
-    fetch("https://api.playdragonfly.net/cookie/register", {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
-    }).then(result => result.json())
-        .then(result => {
-            if (result.success) {
-                console.log(result);
-                Toast.fire({
-                    icon: 'success',
-                    title: 'Account successfully created!'
-                })
-                document.getElementById('id02').style.display = 'none'
-            } else {
-                const error = result.error
-                console.log(result);
-                Toast.fire({
-                    icon: 'error',
-                    title: result.error
-                })
-            }
-            afterLogin(result.success, result.username)
-        })
-});
+        fetch("https://api.playdragonfly.net/cookie/register", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: 'include'
+        }).then(result => result.json())
+            .then(result => {
+                if (result.success) {
+                    console.log(result);
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Account successfully created!'
+                    })
+                } else {
+                    const error = result.error
+                    console.log(result);
+                    Toast.fire({
+                        icon: 'error',
+                        title: result.error
+                    })
+                }
+                afterLogin(result.success, result.username)
+            })
+    });
 
-function switchAuth(auth) {
-    if (auth === 'login') {
-        document.getElementById('id02').style.display = 'none'
-        document.getElementById('id01').style.display = 'block'
-    } else {
-        document.getElementById('id01').style.display = 'none'
-        document.getElementById('id02').style.display = 'block'
+    function switchAuth(auth) {
+        if (auth === 'login') {
+            document.getElementById('id02').style.display = 'none'
+            document.getElementById('id01').style.display = 'block'
+        } else {
+            document.getElementById('id01').style.display = 'none'
+            document.getElementById('id02').style.display = 'block'
+        }
     }
 }
