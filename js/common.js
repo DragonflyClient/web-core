4d2b21965f8dd09e4b6758fcfee277658b3aac60
flaps <julianwening4@gmail.com>
2020-08-21 13:23:27 +0200

    🐛 Fix a bug with the dropdown menu

--- js/common.js
+++ js/common.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-window.onload = () => {
-  getDragonflyAccount();
-};
-/* Dropdown menu
- When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById('myDropdown').classList.toggle('show-acc-dropdown');
-=======
 
 document.addEventListener('DOMContentLoaded', function () {
     const savedUser = localStorage.getItem('user')
@@ -27,22 +17,10 @@ document.addEventListener('DOMContentLoaded', function () {
 toggle between hiding and showing the dropdown content */
 function toggleDropdown(element) {
     element.nextElementSibling.classList.toggle("show-acc-dropdown");
->>>>>>> 3c4abe0636d7368d84364c53bd149f33d938d58c
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
-<<<<<<< HEAD
-  console.log(event.target);
-  if (!event.target.matches('.drgn-information')) {
-    var dropdowns = document.getElementsByClassName('dropdown-content');
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show-acc-dropdown')) {
-        openDropdown.classList.remove('show-acc-dropdown');
-      }
-=======
     if (!event.target.matches('.drgn-information')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
@@ -52,36 +30,9 @@ window.onclick = function (event) {
                 openDropdown.classList.remove('show-acc-dropdown');
             }
         }
->>>>>>> 3c4abe0636d7368d84364c53bd149f33d938d58c
     }
-  }
-};
+}
 function getDragonflyAccount() {
-<<<<<<< HEAD
-  fetch('https://api.playdragonfly.net/cookie/auth', {
-    method: 'POST',
-    credentials: 'include',
-  }).then((res) => {
-    if (res.status === 200) {
-      res.json().then((res) => {
-        const accNameIcon = document.getElementById('drgn-accountname-icon');
-        if (res.success) {
-          console.log(res.username);
-          document.getElementById('drgn-accountname').innerText = res.username;
-
-          document.querySelector('.dropdown-account').style.display = 'inline-block';
-          accNameIcon.style.display = 'inline-block';
-        } else {
-          console.log(res.error);
-          document.getElementById('drgn-accountname').innerText = 'Log in';
-
-          document.querySelector('.dropdown-account').style.display = 'inline-block';
-          document.getElementById('myDropdown').remove();
-          accNameIcon.remove();
-          document
-            .getElementById('drgn-account-login')
-            .setAttribute('href', 'https://playdragonfly.net/login');
-=======
     const accNameIcon = document.getElementById('drgn-accountname-icon')
     const savedUser = localStorage.getItem('user')
     fetch("https://api.playdragonfly.net/cookie/auth", {
@@ -105,11 +56,8 @@ function getDragonflyAccount() {
                     document.getElementById('drgn-account-login').setAttribute('href', `https://playdragonfly.net/login?ref=${window.location.href}`)
                 }
             })
->>>>>>> 3c4abe0636d7368d84364c53bd149f33d938d58c
         }
-      });
-    }
-  });
+    })
 }
 
 /* Set parameter from page before */
@@ -121,51 +69,6 @@ if (window.location.href.indexOf('login') > -1) {
 
 /* Dragonfly account logout */
 function logOut() {
-<<<<<<< HEAD
-  fetch('https://api.playdragonfly.net/cookie/logout', {
-    method: 'POST',
-    credentials: 'include',
-  }).then((res) => {
-    if (res.status === 200) {
-      res.json().then((res) => {
-        if (res.success) {
-          console.log(res);
-          location.reload();
-        }
-      });
-    }
-  });
-}
-
-/* ham nav */
-window.addEventListener('load', function () {
-  const nav = document.querySelector('.nav'),
-    ham = document.querySelector('.ham-wrapper'),
-    socials = document.querySelector('.socials');
-  function toggleNav() {
-    nav.classList.contains('nav-active')
-      ? setTimeout(function () {
-          (nav.style.display = 'none'), (nav.style.position = 'fixed');
-        }, 350)
-      : (nav.classList.contains('dfo') && nav.classList.remove('dfo'),
-        (nav.style.display = 'flex'),
-        (nav.style.position = 'sticky')),
-      setTimeout(function () {
-        nav.classList.toggle('nav-active'),
-          ham.classList.toggle('ham-active'),
-          socials.classList.toggle('socials-active');
-      }, 5);
-  }
-  if (nav.classList.contains('nav-active')) {
-    nav.classList.remove('nav-active');
-    ham.classList.remove('ham-active');
-  }
-  ham.addEventListener('click', toggleNav);
-  function closeMenu(e) {
-    nav.classList.remove('nav-active'), ham.classList.remove('ham-active');
-  }
-});
-=======
 
     fetch("https://api.playdragonfly.net/cookie/logout", {
         method: 'POST',
@@ -187,4 +90,3 @@ function scrollToTop() {
     }, 400)
     closeMenu()
 }
->>>>>>> 3c4abe0636d7368d84364c53bd149f33d938d58c
