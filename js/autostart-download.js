79308ae15ef413ce7e96fe659ea0493e21f4d03a
flaps <julianwening4@gmail.com>
2020-11-20 17:03:52 +0100

    🐛 Only autostart download on first visit

--- js/autostart-download.js
+++ js/autostart-download.js
@@ -1,11 +1,14 @@
 // Autostart Download
-window.addEventListener("load", () => {
-    const downloadLink = $("a[data-download]")[0];
+window.addEventListener('load', () => {
+  const downloadLink = $('a[data-download]')[0];
+  if (!localStorage.getItem('visitedDownload')) {
     setTimeout(() => {
-        window.location = downloadLink.getAttribute("href");
+      window.location = downloadLink.getAttribute('href');
     }, 1000);
 
-    fetch("https://api.playdragonfly.net/v1/analytics/download", {
-        method: "POST"
-    })
+    fetch('https://api.playdragonfly.net/v1/analytics/download', {
+      method: 'POST',
+    });
+    localStorage.setItem('visitedDownload', true);
+  }
 });
